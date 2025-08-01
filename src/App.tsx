70bafac065a70fd4c223f316cc9f589c46076fc2
firatyedibela <yedibelafirat0@gmail.com>
2025-08-02 00:01:45 +0300

    add responsive styles for services section

--- src/App.tsx
+++ src/App.tsx
@@ -11,26 +11,27 @@ function App() {
         <Hero />
       </div>
       <main className="font-body">
-        <section className="services min-h-[100vh] px-[25px] py-16">
+        <section className="services min-h-[100vh] px-[25px] py-28">
           <div className="services-wrap max-w-[1160px] m-auto border border-white h-full flex flex-col items-center gap-12">
             <div className="services-header flex flex-col gap-1">
-              <h2 className="text-[16px] tracking-[2px] font-bold text-golden font-body text-center">
+              <h2 className="text-[16px] lg:text-[20px] tracking-[2px] font-bold text-golden font-body text-center">
                 HİZMETLERİMİZ
               </h2>
-              <p className="text-[28px] text-center font-semibold opacity-90">
+              <p className="lg:w-[1050px] text-[28px] lg:text-[32px] text-center font-semibold opacity-90">
                 Misafirlerimizin tarzını ve beklentilerini anlayarak, en
                 kaliteli hizmeti sunmak için özenle çalışıyoruz.
               </p>
-              <div className="w-[250px] h-[1px] mt-2 bg-black opacity-[0.15] self-center"></div>
+              <div className="w-[250px] md:w-[450px] lg:w-[800px] h-[1px] mt-2 bg-black opacity-[0.15] self-center"></div>
             </div>
 
-            <div className="grid gap-16 justify-center">
+            <div className="grid gap-12 justify-center lg:justify-between lg:grid-cols-2">
               {services.map((service) => (
                 <Service key={service.title} {...service} />
               ))}
             </div>
           </div>
         </section>
+        <section className="barbers min-h-[100vh] px-[25px] py-16"></section>
       </main>
     </div>
   );
