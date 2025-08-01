4b4ffe89d0476b75dae9cc539b647bae8b5edb06
firatyedibela <yedibelafirat0@gmail.com>
2025-08-01 18:46:23 +0300

    implement Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import { Squash as Hamburger } from 'hamburger-react';
+import agLogo from '../assets/A&G.svg';
+
+export const Header = () => {
+  return (
+    <header className="px-3 md:px-8 lg:px-10 h-[72px] md:h-[96px] flex justify-between items-center bg-black">
+      <div className="logo">
+        <img
+          className="h-[28px] lg:h-[34px]"
+          src={agLogo}
+          alt="A&G barber logo"
+        />
+      </div>
+      <Hamburger color="white" size={30} />
+    </header>
+  );
+};
