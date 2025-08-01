6f9e6ad896ffebc727bc3675130c6e9eae391aab
firatyedibela <yedibelafirat0@gmail.com>
2025-08-01 20:13:30 +0300

    adjust font size and border

---
+++ src/components/buttons/AppointmentButton.tsx
@@ -0,0 +1,7 @@
+export const AppointmentButton = () => {
+  return (
+    <button className="w-full max-w-[348px] h-[50px] border-1 border-[#FFE6BD] flex justify-center items-center text-[13px] font-medium uppercase tracking-[2px] text-[#FFE6BD]">
+      Randevu Al
+    </button>
+  );
+};
