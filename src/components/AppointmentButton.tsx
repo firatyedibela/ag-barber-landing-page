35fc032929123ae941c377facf8f0c40e073d89b
firatyedibela <yedibelafirat0@gmail.com>
2025-08-01 20:15:03 +0300

    move button component into buttons folder

--- src/components/AppointmentButton.tsx
+++
@@ -1,7 +0,0 @@
-export const AppointmentButton = () => {
-  return (
-    <button className="w-full max-w-[348px] h-[59px] border-2 border-[#FFE6BD] flex justify-center items-center text-[20px] font-bold tracking-[2px] text-[#FFE6BD]">
-      Randevu Al
-    </button>
-  );
-};
