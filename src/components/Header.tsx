5093390acbe81f1080946e7ad382b583353dc436
firatyedibela <yedibelafirat0@gmail.com>
2025-08-01 20:14:26 +0300

    set horizontal padding to 25px

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import agLogo from '../assets/A&G.svg';
 
 export const Header = () => {
   return (
-    <header className="px-3 md:px-8 lg:px-10 h-[72px] md:h-[96px] flex justify-between items-center bg-black">
+    <header className="px-[25px] md:px-8 lg:px-10 h-[72px] md:h-[96px] flex justify-between items-center bg-black">
       <div className="logo">
         <img
           className="h-[28px] lg:h-[34px]"
