2480eb431254f3e9c5494597c2eb3f09204b0954
firatyedibela <yedibelafirat0@gmail.com>
2025-08-02 20:39:20 +0300

    add reviews array

---
+++ src/data/reviews.tsx
@@ -0,0 +1,50 @@
+import type { Review } from '../types';
+
+export const reviews: Review[] = [
+  {
+    id: 1,
+    avatar: '',
+    owner: 'Metehan Ar',
+    stars: 5,
+    timeAgo: 'bir ay önce',
+    review:
+      'Müşteriye karşı ilgi alaka çok iyi güler yüzlü bir abimiz işinde çok iyi',
+    href: 'https://share.google/MQ3Jga3uLhGvTAvRH',
+  },
+  {
+    id: 2,
+    avatar: '../assets/review-avatars/ali-can.png',
+    owner: 'Ahmet Berkay Yevimli',
+    stars: 5,
+    timeAgo: '3 yıl önce',
+    review: 'Mamak ta en guzel temiz erkek kuaförü AG erkek kuaförü',
+    href: 'https://share.google/dyTmAkDY32dd17UR9',
+  },
+  {
+    id: 3,
+    avatar: '../assets/review-avatars/ali-can.png',
+    owner: 'Ali Can',
+    stars: 5,
+    timeAgo: '3 yıl önce',
+    review: 'Çok başarılı bir kuaför tavsiye ediyorum',
+    href: 'https://share.google/gzQONMhnIlLgdRcUl',
+  },
+  {
+    id: 4,
+    avatar: '../assets/review-avatars/emre-kumsal.png',
+    owner: 'Emre Kumsal',
+    stars: 5,
+    timeAgo: '2 yıl önce',
+    review: 'ADAM HAKINI VERIYOR ELİNE SAĞLIK ABİDİN USTA 👍',
+    href: 'https://share.google/pPrd20gDAc1HGcZy0',
+  },
+  {
+    id: 5,
+    avatar: '../assets/review-avatars/yunus-emre-hıdır.png',
+    owner: 'Yunus Emre Hıdır',
+    stars: 5,
+    timeAgo: 'bir yıl önce',
+    review: 'İşinin ehli',
+    href: 'https://share.google/ifpwZRisZk6Jo6jE7',
+  },
+];
