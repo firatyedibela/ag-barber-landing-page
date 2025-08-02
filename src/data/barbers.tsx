3867e65d1cfe401fa5b92225642223b98774abbe
firatyedibela <yedibelafirat0@gmail.com>
2025-08-02 20:10:06 +0300

    add barbers data

---
+++ src/data/barbers.tsx
@@ -0,0 +1,22 @@
+import type { Barber } from '../types';
+
+export const barbers: Barber[] = [
+  {
+    avatar: 'x',
+    name: 'Abidin Özdemir',
+    title: 'Kıdemli Uzman Berber',
+    introduction:
+      '15 yıl önce kurduğu A&G Erkek Kuaförü’nü hâlâ aynı özen, aynı ustalık ve yılların tecrübesiyle yaşatıyor; her tıraşta bu birikimin izini hissettiriyor.',
+    instagram: 'abidin1234567',
+    phone: '(599) 999 99 99',
+  },
+  {
+    avatar: 'x',
+    name: 'Serkan Eryurt',
+    title: 'Uzman Berber',
+    introduction:
+      '15 yıl önce kurduğu A&G Erkek Kuaförü’nü hâlâ aynı özen, aynı ustalık ve yılların tecrübesiyle yaşatıyor; her tıraşta bu birikimin izini hissettiriyor.',
+    instagram: 'serkan1234567',
+    phone: '(599) 999 99 99',
+  },
+];
