3772cca6986887e619fdfeda2270484e2242033c
firatyedibela <yedibelafirat0@gmail.com>
2025-08-06 21:32:35 +0300

    render Lightbox on slide click

--- src/components/swipers/Gallery.tsx
+++ src/components/swipers/Gallery.tsx
@@ -1,3 +1,7 @@
+import { useState } from 'react';
+
+import { Lightbox } from '../Lightbox';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Navigation } from 'swiper/modules';
 
@@ -18,35 +22,49 @@ const barbershopImages = [
 ];
 
 export const Gallery = () => {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const handleLightboxClose = () => {
+    setSelectedImage(null);
+  };
+
   return (
-    <Swiper
-      className="relative max-w-[380px] md:max-w-[768px] lg:max-w-full"
-      modules={[Navigation, Autoplay]}
-      navigation
-      loop
-      speed={750}
-      spaceBetween={20}
-      slidesPerView={1}
-      breakpoints={{
-        768: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-        },
-      }}
-    >
-      {barbershopImages.map((image) => (
-        <SwiperSlide className="barbershop-slide" key={image}>
-          <div className="m-auto w-full max-w-[380px] h-[250px] lg:max-w-[480px] lg:h-[310px]  text-[72px] font-bold flex justify-center items-center overflow-hidden hover:opacity-80 transition duration-300 hover:scale-105">
-            <img
-              className="w-full h-full object-cover object-center cursor-pointer"
-              src={image}
-              alt="barbershop image"
-            />
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      <Swiper
+        className="relative max-w-[380px] md:max-w-[768px] lg:max-w-full"
+        modules={[Navigation, Autoplay]}
+        navigation
+        loop
+        speed={750}
+        spaceBetween={20}
+        slidesPerView={1}
+        breakpoints={{
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+        }}
+      >
+        {barbershopImages.map((image) => (
+          <SwiperSlide className="barbershop-slide" key={image}>
+            <div
+              className="m-auto w-full max-w-[380px] h-[250px] lg:max-w-[480px] lg:h-[310px]  text-[72px] font-bold flex justify-center items-center overflow-hidden hover:opacity-80 transition duration-300 hover:scale-105"
+              onClick={() => setSelectedImage(image)}
+            >
+              <img
+                className="w-full h-full object-cover object-center cursor-pointer"
+                src={image}
+                alt="barbershop image"
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      {selectedImage && (
+        <Lightbox image={selectedImage} onClose={handleLightboxClose} />
+      )}
+    </>
   );
 };
