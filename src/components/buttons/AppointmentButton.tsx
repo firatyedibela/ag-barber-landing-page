dcbcb80e570f4fce6ebb6f5652ef400bb0fe06d2
firatyedibela <yedibelafirat0@gmail.com>
2025-08-07 11:53:46 +0300

    add btn-animate custom class for animations

--- src/components/buttons/AppointmentButton.tsx
+++ src/components/buttons/AppointmentButton.tsx
@@ -2,7 +2,7 @@ export const AppointmentButton = () => {
   return (
     <a
       href="tel:+905369932365"
-      className="appointment-button relative overflow-hidden w-full max-w-[320px] h-[50px] border-1 border-[#FFE6BD] flex justify-center items-center text-[13px] font-medium uppercase tracking-[2px] text-[#FFE6BD] cursor-pointer hover:shadow-xl"
+      className="btn-animate relative overflow-hidden w-full max-w-[320px] h-[50px] border-1 border-[#FFE6BD] flex justify-center items-center text-[13px] font-medium uppercase tracking-[2px] text-[#FFE6BD] cursor-pointer hover:shadow-xl"
     >
       <span className="z-10">Randevu Al</span>
     </a>
