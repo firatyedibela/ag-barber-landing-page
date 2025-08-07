a35226945d4976e4432b1c262bc873abf3b1f44a
firatyedibela <yedibelafirat0@gmail.com>
2025-08-07 11:32:51 +0300

    replace button element with a

--- src/components/buttons/AppointmentButton.tsx
+++ src/components/buttons/AppointmentButton.tsx
@@ -1,7 +1,10 @@
 export const AppointmentButton = () => {
   return (
-    <button className="w-full max-w-[348px] h-[50px] border-1 border-[#FFE6BD] flex justify-center items-center text-[13px] font-medium uppercase tracking-[2px] text-[#FFE6BD]">
+    <a
+      href="tel:+905369932365"
+      className="w-full max-w-[320px] h-[50px] border-1 border-[#FFE6BD] flex justify-center items-center text-[13px] font-medium uppercase tracking-[2px] text-[#FFE6BD] cursor-pointer"
+    >
       Randevu Al
-    </button>
+    </a>
   );
 };
