405736ae2c44afc52b052ff484a30f8ddcf37f2b
firatyedibela <yedibelafirat0@gmail.com>
2025-08-07 11:51:07 +0300

    add animation to button

--- src/components/buttons/AppointmentButton.tsx
+++ src/components/buttons/AppointmentButton.tsx
@@ -2,9 +2,9 @@ export const AppointmentButton = () => {
   return (
     <a
       href="tel:+905369932365"
-      className="w-full max-w-[320px] h-[50px] border-1 border-[#FFE6BD] flex justify-center items-center text-[13px] font-medium uppercase tracking-[2px] text-[#FFE6BD] cursor-pointer"
+      className="appointment-button relative overflow-hidden w-full max-w-[320px] h-[50px] border-1 border-[#FFE6BD] flex justify-center items-center text-[13px] font-medium uppercase tracking-[2px] text-[#FFE6BD] cursor-pointer hover:shadow-xl"
     >
-      Randevu Al
+      <span className="z-10">Randevu Al</span>
     </a>
   );
 };
