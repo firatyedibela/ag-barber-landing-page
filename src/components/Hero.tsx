077c77d7704e5939306d946c96b03329252f8f6e
firatyedibela <yedibelafirat0@gmail.com>
2025-08-07 09:23:27 +0300

    adjust h1 opacity

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ export const Hero = () => {
   return (
     <div className="hero h-[calc(100vh-72px)] md:h-[calc(100vh-96px)] border-red-500 font-body relative">
       <div className="text-cream flex flex-col items-center sm:items-start text-center sm:text-start px-[25px] lg:w-[85%] m-auto pt-20 md:pt-20 lg:pt-40">
-        <h1 className="text-[16px] lg:text-[20px] xl:text-[26px] font-medium tracking-[4px] opacity-50 mb-2">
+        <h1 className="text-[16px] lg:text-[20px] xl:text-[26px] font-medium tracking-[4px] opacity-65 mb-2">
           ERKEK KUAFÖRÜ
         </h1>
         <p className="max-w-[550px] xl:max-w-[770px] text-[24px] lg:text-[30px] xl:text-[36px] font-semibold mb-12">
