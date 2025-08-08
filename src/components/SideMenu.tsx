0de56a651756df93daef40f6120a85d6741387b5
firatyedibela <yedibelafirat0@gmail.com>
2025-08-08 05:50:50 +0300

    implement SideMenu component

---
+++ src/components/SideMenu.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { Squash as Hamburger } from 'hamburger-react';
+import { useClickAway } from '@uidotdev/usehooks';
+import clsx from 'clsx';
+
+type SideMenuProps = {
+  routes: string[];
+};
+
+export const SideMenu = ({ routes }: SideMenuProps) => {
+  const [isOpen, setOpen] = useState<boolean>(false);
+
+  const ref = useClickAway<HTMLDivElement>(() => {
+    setOpen(false);
+  });
+
+  return (
+    <div ref={ref} className="relative h-full flex items-center">
+      <button
+        onClick={() => setOpen((prev) => !prev)}
+        aria-expanded={isOpen}
+        className={clsx(
+          'menu-button sm:pl-2 h-[42px] cursor-pointer border flex items-center font-bold text-sm tracking-[2px] bg-black fixed right-[25px] md:right-8 lg:right-10 z-15',
+          isOpen && ' border-white/50'
+        )}
+      >
+        <span className="text-[#b8a269] hidden sm:inline">MENU</span>
+        <Hamburger toggled={isOpen} color="white" size={25} />
+      </button>
+      {isOpen && (
+        <div className="fixed w-[280px] bottom-0 top-0 bg-[#0C0C0C] right-0 z-10 font-body">
+          <ul className="text-white flex flex-col gap-2 pt-32 pl-8">
+            {routes.map((route) => (
+              <li key={route}>
+                <button className="nav-btn capitalize text-lg font-semibold cursor-pointer py-2">
+                  {route}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
