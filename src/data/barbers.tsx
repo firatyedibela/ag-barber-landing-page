a02dc53e09d223925f569fc6efcc0c520f8801ed
firatyedibela <yedibelafirat0@gmail.com>
2025-08-08 11:20:06 +0300

    add correct instagram and phone

--- src/data/barbers.tsx
+++ src/data/barbers.tsx
@@ -7,8 +7,8 @@ export const barbers: Barber[] = [
     title: 'Kıdemli Uzman Berber',
     introduction:
       '15 yıl önce kurduğu A&G Erkek Kuaförü’nü hâlâ aynı özen, aynı ustalık ve yılların tecrübesiyle yaşatıyor; her tıraşta bu birikimin izini hissettiriyor.',
-    instagram: 'abidin1234567',
-    phone: '(599) 999 99 99',
+    instagram: 'https://instagram.com',
+    phone: '+905369932365',
   },
   {
     avatar: 'src/assets/pp.png',
@@ -16,7 +16,7 @@ export const barbers: Barber[] = [
     title: 'Uzman Berber',
     introduction:
       'Çok genç yaşlarda başlayıp yıllar içinde ustalaştığı berberlik mesleğini, müşteri memnuniyetine verdiği önem ve tutkuyla sürdürüyor.',
-    instagram: 'serkan1234567',
-    phone: '(599) 999 99 99',
+    instagram: 'https://instagram.com',
+    phone: '+905369932365',
   },
 ];
