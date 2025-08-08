e8febcbace1216063ca35bc4288f627034b31d65
firatyedibela <yedibelafirat0@gmail.com>
2025-08-08 07:27:16 +0300

    add animations

--- src/components/buttons/AppointmentButton.tsx
+++ src/components/buttons/AppointmentButton.tsx
@@ -1,10 +1,16 @@
+import { motion } from 'motion/react';
+
 export const AppointmentButton = () => {
   return (
-    <a
+    <motion.a
+      initial={{ y: 150, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      transition={{ duration: 1, delay: 0.6 }}
+      viewport={{ once: true }}
       href="tel:+905369932365"
       className="btn-animate w-full max-w-[320px] h-[50px] border-1 border-current flex justify-center items-center text-[13px] font-medium uppercase tracking-[2px] text-[#FFE6BD]"
     >
       <span>Randevu Al</span>
-    </a>
+    </motion.a>
   );
 };
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,5 +1,6 @@
 import { ScrollDownButton } from '../buttons/ScrollDownButton';
 import { AppointmentButton } from '../buttons/AppointmentButton';
+import { motion } from 'motion/react';
 
 export const Hero = () => {
   return (
@@ -7,20 +8,38 @@ export const Hero = () => {
       id="section-hero"
       className="hero h-[100vh] border-red-500 font-body relative"
     >
-      <div className="text-cream flex flex-col items-center sm:items-start text-center sm:text-start px-[25px] lg:w-[85%] m-auto pt-42 lg:pt-56">
-        <h1 className="text-[16px] lg:text-[20px] xl:text-[26px] font-medium tracking-[4px] opacity-65 mb-2">
+      <motion.div className="text-cream flex flex-col items-center sm:items-start text-center sm:text-start px-[25px] lg:w-[85%] m-auto pt-42 lg:pt-56">
+        <motion.h1
+          initial={{ y: 150, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 0.65 }}
+          transition={{ duration: 1.2 }}
+          viewport={{ once: true }}
+          className="text-[16px] lg:text-[20px] xl:text-[26px] font-medium tracking-[4px] opacity-65 mb-2"
+        >
           ERKEK KUAFÖRÜ
-        </h1>
-        <p className="max-w-[550px] xl:max-w-[770px] text-[24px] lg:text-[30px] xl:text-[36px] font-semibold mb-12">
+        </motion.h1>
+        <motion.p
+          initial={{ y: 150, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1.2, delay: 0.3 }}
+          viewport={{ once: true }}
+          className="max-w-[550px] xl:max-w-[770px] text-[24px] lg:text-[30px] xl:text-[36px] font-semibold mb-12"
+        >
           A & G olarak, profesyonel saç ve sakal bakımında güvenilir
           hizmetimizle sizleri de salonumuza bekliyoruz.
-        </p>
+        </motion.p>
         <AppointmentButton />
-        <div className="flex flex-col gap-2 items-center absolute bottom-10 left-1/2 -translate-x-1/2">
-          <span>Daha Fazla Bilgi</span>
-          <ScrollDownButton />
-        </div>
-      </div>
+      </motion.div>
+      <motion.div
+        initial={{ bottom: '-30px', opacity: 0 }}
+        whileInView={{ bottom: '40px', opacity: 1 }}
+        transition={{ duration: 0.6, delay: 1 }}
+        viewport={{ once: true }}
+        className="flex flex-col gap-2 items-center absolute bottom-10 left-1/2 -translate-x-1/2"
+      >
+        <span className="text-cream font-medium">Daha Fazla Bilgi</span>
+        <ScrollDownButton />
+      </motion.div>
     </div>
   );
 };
