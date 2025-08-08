7b0a8e828dc77759371775e99c43c1858bcb269e
firatyedibela <yedibelafirat0@gmail.com>
2025-08-08 05:50:29 +0300

    remove header bg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
-import { Squash as Hamburger } from 'hamburger-react';
+import { SideMenu } from './SideMenu';
 import agLogo from '../assets/A&G.svg';
 
 export const Header = () => {
   return (
-    <header className="px-[25px] md:px-8 lg:px-10 h-[72px] md:h-[96px] flex justify-between items-center bg-black">
+    <header className="absolute z-20 px-[25px] md:px-8 lg:px-10 h-[72px] md:h-[96px] flex justify-between items-center ">
       <div className="logo">
         <img
           className="h-[28px] lg:h-[34px]"
@@ -11,7 +11,16 @@ export const Header = () => {
           alt="A&G barber logo"
         />
       </div>
-      <Hamburger color="white" size={30} />
+      <SideMenu
+        routes={[
+          'ana sayfa',
+          'hizmetlerimiz',
+          'berberlerimiz',
+          'galeri',
+          'ulaşım',
+          'İletişim',
+        ]}
+      />
     </header>
   );
 };
