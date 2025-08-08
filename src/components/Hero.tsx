88da8cca7864edf1a97fe4e6e6f5255f03babaee
firatyedibela <yedibelafirat0@gmail.com>
2025-08-08 05:50:38 +0300

    make hero section 100vh height

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,8 +3,8 @@ import { AppointmentButton } from './buttons/AppointmentButton';
 
 export const Hero = () => {
   return (
-    <div className="hero h-[calc(100vh-72px)] md:h-[calc(100vh-96px)] border-red-500 font-body relative">
-      <div className="text-cream flex flex-col items-center sm:items-start text-center sm:text-start px-[25px] lg:w-[85%] m-auto pt-20 md:pt-20 lg:pt-40">
+    <div className="hero h-[100vh] border-red-500 font-body relative">
+      <div className="text-cream flex flex-col items-center sm:items-start text-center sm:text-start px-[25px] lg:w-[85%] m-auto pt-42 lg:pt-56">
         <h1 className="text-[16px] lg:text-[20px] xl:text-[26px] font-medium tracking-[4px] opacity-65 mb-2">
           ERKEK KUAFÖRÜ
         </h1>
