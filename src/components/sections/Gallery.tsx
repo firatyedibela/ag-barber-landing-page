13735c38debf4d0a959e693caf250bcd0cefb907
firatyedibela <yedibelafirat0@gmail.com>
2025-08-08 07:54:17 +0300

    adjust gallery animations

--- src/components/sections/Gallery.tsx
+++ src/components/sections/Gallery.tsx
@@ -5,14 +5,14 @@ import { motion } from 'motion/react';
 export const Gallery = () => {
   return (
     <section id="section-gallery" className="gallery min-h-[100vh] py-24 ">
-      <motion.div
-        initial={{ y: 150, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.8 }}
-        viewport={{ once: true }}
-        className="gallery-wrap m-auto"
-      >
-        <div className="gallery-header flex flex-col max-w-[1160px] m-auto items-center text-center gap-2 px-4 mb-8 lg:mb-16">
+      <motion.div className="gallery-wrap m-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: true, amount: 0.4 }}
+          className="gallery-header flex flex-col max-w-[1160px] m-auto items-center text-center gap-2 px-4 mb-8 lg:mb-16"
+        >
           <h2 className="text-[24px] lg:text-[64px] font-semibold">
             Salonumuzdan Görseller
           </h2>
@@ -21,13 +21,19 @@ export const Gallery = () => {
             ikramlarımızla keyfinize keyif katıyoruz.
           </p>
           <div className="w-[250px] md:w-[450px] lg:w-[800px] h-[1px] mt-2 bg-black opacity-[0.15] self-center"></div>
-        </div>
+        </motion.div>
         <div className="image-gallery-wrap mb-6 max-w-[1440px] m-auto md:px-4">
           <BarbershopSwiper />
         </div>
-        <div className="reviews-wrap max-w-[1160px] xl:max-w-[1280px] md:px-8 m-auto">
+        <motion.div
+          initial={{ y: 150, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: true, amount: 0.4 }}
+          className="reviews-wrap max-w-[1160px] xl:max-w-[1280px] md:px-8 m-auto"
+        >
           <ReviewsSwiper />
-        </div>
+        </motion.div>
       </motion.div>
     </section>
   );
