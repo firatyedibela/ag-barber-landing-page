f6cc9b533fb840cbd70eaa143098586f33ba0f2d
firatyedibela <yedibelafirat0@gmail.com>
2025-08-11 17:19:30 +0300

    update mock data

--- src/data/barbers.tsx
+++ src/data/barbers.tsx
@@ -4,7 +4,7 @@ import profileImage from '../assets/pp.png';
 export const barbers: Barber[] = [
   {
     avatar: profileImage,
-    name: 'Abidin Özdemir',
+    name: 'Abidin Berber',
     title: 'Kıdemli Uzman Berber',
     introduction:
       '15 yıl önce kurduğu A&G Erkek Kuaförü’nü hâlâ aynı özen, aynı ustalık ve yılların tecrübesiyle yaşatıyor; her tıraşta bu birikimin izini hissettiriyor.',
@@ -13,7 +13,7 @@ export const barbers: Barber[] = [
   },
   {
     avatar: profileImage,
-    name: 'Serkan Eryurt',
+    name: 'Serkan Berber',
     title: 'Uzman Berber',
     introduction:
       'Çok genç yaşlarda başlayıp yıllar içinde ustalaştığı berberlik mesleğini, müşteri memnuniyetine verdiği önem ve tutkuyla sürdürüyor.',
